cf60c26ef6f9d541c4d7af82806d974c53821800
Nikita Fedorov <fedorov@ispras.ru>
2025-04-28 16:29:10 +0300

    added 11 and 12

--- 04_arithmetics_and_pipelining/04_12_float_discriminant_distributor/04_12_float_discriminant_distributor.sv
+++ 04_arithmetics_and_pipelining/04_12_float_discriminant_distributor/04_12_float_discriminant_distributor.sv
@@ -30,5 +30,77 @@ module float_discriminant_distributor (
     // Note 2:
     // Latency of the module "float_discriminant" should be clarified from the waveform.
 
+    localparam N = 50;  
+
+    logic [FLEN-1:0] in_a   [N];
+    logic [FLEN-1:0] in_b   [N];
+    logic [FLEN-1:0] in_c   [N];
+    logic            in_vld [N];
+    logic [N-1:0]    instance_busy;
+
+    logic [FLEN-1:0] out_res         [N];
+    logic            out_vld         [N];
+    logic            out_res_negative[N];
+    logic            out_err         [N];
+
+    logic [$clog2(N)-1:0] cnt;
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            cnt <= '0;
+        end else if (arg_vld) begin
+            // Ищем свободный экземпляр
+            for (int i = 0; i < N; i++) begin
+                logic [$clog2(N)-1:0] next_cnt = (cnt + i) % N;
+                if (!instance_busy[next_cnt]) begin
+                    cnt <= next_cnt;
+                    break;
+                end
+            end
+        end
+    end
+
+    always_comb begin
+        for (int i = 0; i < N; i++) begin
+            in_vld[i] = '0;
+            in_a[i]   = '0;
+            in_b[i]   = '0;
+            in_c[i]   = '0;
+        end
+
+        if (arg_vld && !instance_busy[cnt]) begin
+            in_a[cnt]   = a;
+            in_b[cnt]   = b;
+            in_c[cnt]   = c;
+            in_vld[cnt] = arg_vld;
+        end
+    end
+
+    always_comb begin
+        res_vld      = out_vld[cnt];
+        res          = out_res[cnt];
+        res_negative = out_res_negative[cnt];
+        err          = out_err[cnt];
+        busy         = instance_busy[cnt];
+    end
+
+    generate
+        for (genvar i = 0; i < N; i++) begin
+            float_discriminant inst (
+                .clk(clk),
+                .rst(rst),
+                .arg_vld(in_vld[i]),
+                .a(in_a[i]),
+                .b(in_b[i]),
+                .c(in_c[i]),
+                .res_vld(out_vld[i]),
+                .res(out_res[i]),
+                .res_negative(out_res_negative[i]),
+                .err(out_err[i]),
+                .busy(instance_busy[i])
+            );
+        end
+    endgenerate
+
 
 endmodule
